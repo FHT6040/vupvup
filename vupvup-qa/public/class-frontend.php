4e593e15ff7f3deb3d9a9f6e24f9220e819c9251
Frank Tessin <frank.tessin@gmail.com>
2026-03-31 20:40:43 +0200

    Auto-save: 2026-03-31 20:40:43

--- vupvup-qa/public/class-frontend.php
+++ vupvup-qa/public/class-frontend.php
@@ -71,7 +71,7 @@ class VupVup_QA_Frontend {
 
     private function render_register(): void {
         if ( is_user_logged_in() ) {
-            wp_redirect( home_url( 'vupvup/dashboard/' ) ); exit;
+            wp_redirect( home_url( 'app/dashboard/' ) ); exit;
         }
         $this->enqueue_dashboard_assets();
         include VUPVUP_QA_DIR . 'public/templates/register.php';
@@ -79,7 +79,7 @@ class VupVup_QA_Frontend {
 
     private function render_login(): void {
         if ( is_user_logged_in() ) {
-            wp_redirect( home_url( 'vupvup/dashboard/' ) ); exit;
+            wp_redirect( home_url( 'app/dashboard/' ) ); exit;
         }
         $this->enqueue_dashboard_assets();
         include VUPVUP_QA_DIR . 'public/templates/login.php';
